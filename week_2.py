ab81ef17d4e140ef3de7cb3871eb0471b9a684af
PrajapatiMeet <59308223+PrajapatiMeet@users.noreply.github.com>
2020-07-13 13:53:29 +0530

    Update week_2.py

--- week_2.py
+++ week_2.py
@@ -2,7 +2,15 @@ import urllib.request, urllib.parse, urllib.error
 import re
 
 fname = input("Enter name of file : ")
-file = urllib.request.urlopen(fname)
+if (len(fname) < 1):
+    fname = 'http://py4e-data.dr-chuck.net/regex_sum_576144.txt'
+
+try:
+    file = urllib.request.urlopen(fname)
+except:
+    print('file not found')
+    quit()
+
 sum = 0
 for line in file:
     line = line.decode().strip()
